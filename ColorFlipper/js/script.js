ef53bf38f6870d253c36afaa1a909abc229386bb
Jaxon Desktop <handtjaxon@gmail.com>
2022-02-14 14:03:44 -0600

    Added the start of the color picker site

---
+++ ColorFlipper/js/script.js
@@ -0,0 +1,6 @@
+const simpleColors = ["green", "red", "rgba(133,122,200)", "#f1f1f1"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector("color");
+
+btn.addEventListener("click")
